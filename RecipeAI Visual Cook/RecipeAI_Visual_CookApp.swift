21a96deea329a5ddfde51f3000b1d7131001a22c
Eric Samitov <samitoveric24@gmail.com>
2024-04-08 18:12:07 -0700

    Initial Commit

---
+++ RecipeAI Visual Cook/RecipeAI_Visual_CookApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAI_Visual_CookApp.swift
+//  RecipeAI Visual Cook
+//
+//  Created by Eric Samitov on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAI_Visual_CookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
